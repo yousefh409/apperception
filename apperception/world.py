72917b0dc411d9f284d6b620a80b9f256346dd71
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-10-14 22:11:00 -0700

    add support for world id and version

--- apperception/world.py
+++ apperception/world.py
@@ -1,4 +1,5 @@
 import copy
+from typing import Dict, List, Optional
 
 import cv2
 import matplotlib.pyplot as plt
@@ -12,12 +13,20 @@ world_executor = WorldExecutor()
 
 
 class World:
-    def __init__(self, name, units, enable_tasm=False):
-        self.VideoContext = VideoContext(name, units)
-        self.MetadataContext = MetadataContext(single_mode=False)
+    __world_counter: int = 0
+    __version_couter: Dict[int, int] = {}
+
+    def __init__(self, name: str, units: str, enable_tasm: bool = False):
+        self.VideoContext: VideoContext = VideoContext(name, units)
+        self.MetadataContext: MetadataContext = MetadataContext(single_mode=False)
         self.MetadataContext.start_time = self.VideoContext.start_time
-        self.GetVideo = False
-        self.enable_tasm = enable_tasm
+        self.GetVideo: bool = False
+        self.enable_tasm: bool = enable_tasm
+        self.parent: Optional[World] = None
+        self.world_id: int = World.__world_counter
+        self.version_id = 0
+        World.__version_couter[self.world_id] = 1
+        World.__world_counter += 1
         # self.AccessedVideoContext = False
 
     def get_camera(self, cam_id=[]):
@@ -44,19 +53,19 @@ class World:
         return self.VideoContext.get_units()
 
     def item(self, item_id, cam_id, item_type, location):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy(new_version=True)
         new_context.VideoContext.item(item_id, cam_id, item_type, location)
         return new_context
 
     def camera(self, cam_id, location, ratio, video_file, metadata_identifier, lens):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy(new_version=True)
         new_context.VideoContext.camera(
             cam_id, location, ratio, video_file, metadata_identifier, lens
         )
         return new_context
 
     def add_properties(self, cam_id, properties, property_type):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy(new_version=True)
         new_context.VideoContext.properties(cam_id, properties, property_type)
         return new_context
 
@@ -67,7 +76,7 @@ class World:
         tracker_type="multi",
         tracker=None,
     ):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy(new_version=True)
         new_context.VideoContext.camera_nodes[cam_id].recognize(algo, tracker_type, tracker)
         return new_context
 
@@ -76,55 +85,55 @@ class World:
     #########################
 
     def get_columns(self, *argv, distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.get_columns(argv, distinct)
         return new_context
 
     def predicate(self, p, evaluated_var={}):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.predicate(p, evaluated_var)
         return new_context
 
     def selectkey(self, distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.selectkey(distinct)
         return new_context
 
     def get_trajectory(self, interval=[], distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.get_trajectory(interval, distinct)
         return new_context
 
     def get_geo(self, interval=[], distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.get_geo(interval, distinct)
         return new_context
 
     def get_time(self, distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.get_time(distinct)
         return new_context
 
     def get_distance(self, interval=[], distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.distance(interval, distinct)
         return new_context
 
     def get_speed(self, interval=[], distinct=False):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.get_speed(interval, distinct)
         return new_context
 
     def get_video(self, cam_id=[]):
         # Go through all the cameras in 'filtered' world and obtain videos
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.GetVideo = True
         # get camera gives the direct results from the data base
         new_context.get_video_cams = self.get_camera(cam_id)
         return new_context
 
     def interval(self, time_interval):
-        new_context = copy.deepcopy(self)
+        new_context = self.__deepcopy()
         new_context.MetadataContext.interval(time_interval)
         return new_context
 
@@ -181,3 +190,26 @@ class World:
             plt.figure()
             plt.imshow(frame)
             plt.show()
+
+    def __deepcopy(self, new_version: bool = False):
+        new_world = copy.deepcopy(self)
+        new_world.parent = self
+
+        if new_version:
+            new_world.version_id = World.__version_couter[self.world_id]
+            World.__version_couter[self.world_id] += 1
+
+        return new_world
+
+    def __get_ancestors(self):
+        world = self
+        ancestors: List[int] = []
+
+        while world:
+            while world.parent and world.version_id == world.parent.version_id:
+                world = world.parent
+
+            ancestors.append(world.version_id)
+            world = world.parent
+
+        return "-".join(reversed(ancestors))
